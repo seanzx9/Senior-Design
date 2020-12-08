acd5a081d1b566c2f5fec5e1b204569a20065dac
seanzx9 <seanzx9@gmail.com>
2020-12-07 23:26:31 -0500

    Updated stitch

--- stitch.m
+++ stitch.m
@@ -1,3 +1,5 @@
+%converts 8 input data matrices to pointcloud objects and then
+%returns single merged point cloud
 function pcMerged = stitch(I0, I45, I90, I135, I180, I225, I270, I315)
     %find largest point cloud for reference point cloud
     [n1, ~] = size(I0);
@@ -14,44 +16,44 @@ function pcMerged = stitch(I0, I45, I90, I135, I180, I225, I270, I315)
     %img 1 data
     xyz = [I0(:, 1), I0(:, 2), I0(:, 3)];
     rgb = [I0(:, 4), I0(:, 5), I0(:, 6)];
-    pc0 = pcdenoise(pointCloud(xyz, 'color', rgb));
+    pc0 = pointCloud(xyz, 'color', rgb);
     
     %img 2 data
     xyz = [I45(:, 1), I45(:, 2), I45(:, 3)];
     rgb = [I45(:, 4), I45(:, 5), I45(:, 6)];
-    pc45 = pcdenoise(pointCloud(xyz, 'color', rgb));
+    pc45 = pointCloud(xyz, 'color', rgb);
     
     %img 3 data
     xyz = [I90(:, 1), I90(:, 2), I90(:, 3)];
     rgb = [I90(:, 4), I90(:, 5), I90(:, 6)];
-    pc90 = pcdenoise(pointCloud(xyz, 'color', rgb));
+    pc90 = pointCloud(xyz, 'color', rgb);
     
     %img 4 data
     xyz = [I135(:, 1), I135(:, 2), I135(:, 3)];
     rgb = [I135(:, 4), I135(:, 5), I135(:, 6)];
-    pc135 = pcdenoise(pointCloud(xyz, 'color', rgb));
+    pc135 = pointCloud(xyz, 'color', rgb);
     
     %img 5 data
     xyz = [I180(:, 1), I180(:, 2), I180(:, 3)];
     rgb = [I180(:, 4), I180(:, 5), I180(:, 6)];
-    pc180 = pcdenoise(pointCloud(xyz, 'color', rgb));
+    pc180 = pointCloud(xyz, 'color', rgb);
     
     %img 6 data
     xyz = [I225(:, 1), I225(:, 2), I225(:, 3)];
     rgb = [I225(:, 4), I225(:, 5), I225(:, 6)];
-    pc225 = pcdenoise(pointCloud(xyz, 'color', rgb));
+    pc225 = pointCloud(xyz, 'color', rgb);
     
     %img 7 data
     xyz = [I270(:, 1), I270(:, 2), I270(:, 3)];
     rgb = [I270(:, 4), I270(:, 5), I270(:, 6)];
-    pc270 = pcdenoise(pointCloud(xyz, 'color', rgb));
+    pc270 = pointCloud(xyz, 'color', rgb);
     
     %img 8 data
     xyz = [I315(:, 1), I315(:, 2), I315(:, 3)];
     rgb = [I315(:, 4), I315(:, 5), I315(:, 6)];
-    pc315 = pcdenoise(pointCloud(xyz, 'color', rgb));
+    pc315 = pointCloud(xyz, 'color', rgb);
     
-    %translate all point clouds
+    %translate all point clouds for alignment
     x = 0.15;
     z = -0.3;
     A = [1, 0, 0, 0;
@@ -131,7 +133,7 @@ function pcMerged = stitch(I0, I45, I90, I135, I180, I225, I270, I315)
          0, 0, 0, 1];
     tform = affine3d(A);
     pc315 = pctransform(pc315, tform);
-    
+        
     %add all clouds to list
     pc = [pc0, pc45, pc90, pc135, pc180, pc225, pc270, pc315];
     
@@ -143,7 +145,7 @@ function pcMerged = stitch(I0, I45, I90, I135, I180, I225, I270, I315)
            pcMerged = pcmerge(pcRef, pc(i), mergeSize); 
         end
     end
-    
+     
     %individual point clouds
     figure;
     subplot(2, 4, 1), dispPtCld(pc(1), '0 degrees');
@@ -157,8 +159,5 @@ function pcMerged = stitch(I0, I45, I90, I135, I180, I225, I270, I315)
     
     %merged point cloud
     figure;
-    pcshow(pcMerged);
-    xlabel('X');
-    ylabel('Y');
-    zlabel('Z');
+    dispPtCld(pcMerged, 'Merged Point Cloud')
 end
