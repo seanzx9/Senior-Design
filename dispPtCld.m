c8662630177ca1fe22393c41690dde26ee581413
seanzx9 <seanzx9@gmail.com>
2020-12-07 22:43:46 -0500

    Improved to handle pointCloud object

--- dispPtCld.m
+++ dispPtCld.m
@@ -1,17 +1,28 @@
 %display point cloud data in 3D plot
 function dispPtCld(data, t)   
-    x = data(:, 1);
-    y = data(:, 2);
-    z = data(:, 3);
+    [~, m] = size(data);
     
-    r = data(:, 4);
-    g = data(:, 5);
-    b = data(:, 6);
-    
-    scatter3(x, z, -y, [], [r, g, b] / 255, '.');
-    grid on;
-    title(t);
-    xlabel('X');
-    ylabel('Z');
-    zlabel('Y');
+    if m == 6
+        x = data(:, 1);
+        y = data(:, 2);
+        z = data(:, 3);
+
+        r = data(:, 4);
+        g = data(:, 5);
+        b = data(:, 6);
+
+        scatter3(x, z, -y, [], [r, g, b] / 255, '.');
+        grid on;
+        title(t);
+        xlabel('X');
+        ylabel('Z');
+        zlabel('Y');
+    else
+        pcshow(data);
+        grid on;
+        title(t);
+        xlabel('X');
+        ylabel('Z');
+        zlabel('Y');        
+    end
 end
