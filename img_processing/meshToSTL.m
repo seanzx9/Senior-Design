0b73e06c1212e82357c15d98945c31c96818667c
seanzx9 <seanzx9@gmail.com>
2021-03-17 19:19:15 -0400

    Cleaned up

--- img_processing/meshToSTL.m
+++ img_processing/meshToSTL.m
@@ -2,7 +2,7 @@
 function meshToSTL(mesh)
     %get facets that make up the boundary of the alpha shape
     [bf, P] = boundaryFacets(mesh);
-        
+    
     %write to STL file
     stlwrite(triangulation(bf, P), 'stlData/res.stl');
 end
