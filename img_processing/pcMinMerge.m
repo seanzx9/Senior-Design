456a81105ed7aba00ca1f9867f4a8f31b8a7879c
seanzx9 <seanzx9@gmail.com>
2021-05-04 16:22:06 -0400

    Cleaned up

--- img_processing/pcMinMerge.m
+++ img_processing/pcMinMerge.m
@@ -3,7 +3,7 @@
 %y = how far in the Y-axis the PC is away from the merged PC
 %pcInput = the PC that needs to be moved to the correct place
 %pcRef = the reference PC that will merge with pcInput
-function pcMin = pcMinMerge(xDist, yDist, pcInput, pcRef)
+function pcMerged = pcMinMerge(xDist, zDist, pcInput, pcRef)
     %given mergeSize
     mergeSize = 0.001;
 
@@ -19,7 +19,7 @@ function pcMin = pcMinMerge(xDist, yDist, pcInput, pcRef)
         A = [1, 0, 0, 0;
              0, 1, 0, 0;
              0, 0, 1, 0;
-             startingX, 0, yDist, 1];
+             startingX, 0, zDist, 1];
         tform = affine3d(A);
         pctest = pctransform(pcInput, tform);
         pcMerged = pcmerge(pcRef, pctest, mergeSize);
@@ -37,44 +37,44 @@ function pcMin = pcMinMerge(xDist, yDist, pcInput, pcRef)
         startingX = startingX + 0.001;
     end
 
-    %find optimal y axis translation
-    startingY = yDist - 0.05; %bottom 5cm boundary
-    minYCount = intmax; %init min value
-    MatrixY = zeros(10,1);
-    SmallestMatrixY = zeros(10,1);
-    indexY = 1;
+    %find optimal z axis translation
+    startingZ = zDist - 0.05; %bottom 5cm boundary
+    minZCount = intmax; %init min value
+    MatrixZ = zeros(10,1);
+    SmallestMatrixZ = zeros(10,1);
+    indexZ = 1;
     
     %start from bottom 5cm boundary and iterate to top 5cm boundary
     for i = 1 : 100
         A = [1, 0, 0, 0;
              0, 1, 0, 0;
              0, 0, 1, 0;
-             xDist, 0, startingY, 1];
+             xDist, 0, startingZ, 1];
         tform = affine3d(A);
         pctest = pctransform(pcInput, tform);
         pcMerged = pcmerge(pcRef, pctest, mergeSize);
-        MatrixY(i) = startingY;
+        MatrixZ(i) = startingZ;
         
         %replace if the temporary merged PC is smaller 
-        if (pcMerged.Count < minYCount)
-            minYCount = pcMerged.Count;
-            yDist = startingY;
-            SmallestMatrixY(indexY) = startingY;
-            indexY = indexY + 1;
+        if (pcMerged.Count < minZCount)
+            minZCount = pcMerged.Count;
+            zDist = startingZ;
+            SmallestMatrixZ(indexZ) = startingZ;
+            indexZ = indexZ + 1;
         end
 
-        %increments startingY by 0.001 to shift PC
-        startingY = startingY + 0.001;
+        %increments startingZ by 0.001 to shift PC
+        startingZ = startingZ + 0.001;
     end
     
     %performs merge with optimal translation values
     A = [1, 0, 0, 0;
          0, 1, 0, 0;
          0, 0, 1, 0;
-         xDist, 0, yDist, 1];
+         xDist, 0, zDist, 1];
     tform = affine3d(A);
     pcInput = pctransform(pcInput, tform);
-
+    
     %returns the merged PC
-    pcMin = pcmerge(pcRef, pcInput, mergeSize);
+    pcMerged = pcmerge(pcRef, pcInput, mergeSize);
 end
--- img_processing/stitch.m
+++ img_processing/stitch.m
@@ -1,54 +1,54 @@
 %converts 8 input data matrices to pointcloud objects and then
 %returns single merged point cloud
-function pcFinal = stitch(I0, I45, I90, I135, I180, I225, I270, I315)
+function pcStitched = stitch(I0, I45, I90, I135, I180, I225, I270, I315)
     %create pointCloud objects from input data
     %img 1 data
     xyz = [I0(:, 1), I0(:, 2), I0(:, 3)];
     rgb = [I0(:, 4), I0(:, 5), I0(:, 6)];
-    pc0 = pointCloud(xyz, 'color', rgb);
+    pc0 = pcdenoise(pointCloud(xyz, 'color', rgb));
     
     %img 2 data
     xyz = [I45(:, 1), I45(:, 2), I45(:, 3)];
     rgb = [I45(:, 4), I45(:, 5), I45(:, 6)];
-    pc45 = pointCloud(xyz, 'color', rgb);
+    pc45 =pcdenoise( pointCloud(xyz, 'color', rgb));
     
     %img 3 data
     xyz = [I90(:, 1), I90(:, 2), I90(:, 3)];
     rgb = [I90(:, 4), I90(:, 5), I90(:, 6)];
-    pc90 = pointCloud(xyz, 'color', rgb);
+    pc90 = pcdenoise(pointCloud(xyz, 'color', rgb));
     
     %img 4 data
     xyz = [I135(:, 1), I135(:, 2), I135(:, 3)];
     rgb = [I135(:, 4), I135(:, 5), I135(:, 6)];
-    pc135 = pointCloud(xyz, 'color', rgb);
+    pc135 = pcdenoise(pointCloud(xyz, 'color', rgb));
     
     %img 5 data
     xyz = [I180(:, 1), I180(:, 2), I180(:, 3)];
     rgb = [I180(:, 4), I180(:, 5), I180(:, 6)];
-    pc180 = pointCloud(xyz, 'color', rgb);
+    pc180 = pcdenoise(pointCloud(xyz, 'color', rgb));
     
     %img 6 data
     xyz = [I225(:, 1), I225(:, 2), I225(:, 3)];
     rgb = [I225(:, 4), I225(:, 5), I225(:, 6)];
-    pc225 = pointCloud(xyz, 'color', rgb);
+    pc225 = pcdenoise(pointCloud(xyz, 'color', rgb));
     
     %img 7 data
     xyz = [I270(:, 1), I270(:, 2), I270(:, 3)];
     rgb = [I270(:, 4), I270(:, 5), I270(:, 6)];
-    pc270 = pointCloud(xyz, 'color', rgb);
+    pc270 = pcdenoise(pointCloud(xyz, 'color', rgb));
     
     %img 8 data
     xyz = [I315(:, 1), I315(:, 2), I315(:, 3)];
     rgb = [I315(:, 4), I315(:, 5), I315(:, 6)];
-    pc315 = pointCloud(xyz, 'color', rgb);
+    pc315 = pcdenoise(pointCloud(xyz, 'color', rgb));
     
     %translate all point clouds for easier alignment later
     x = 0.15;
-    y = -0.3;
+    z = -0.3;
     A = [1, 0, 0, 0;
          0, 1, 0, 0;
          0, 0, 1, 0;
-         x, 0, y, 1];
+         x, 0, z, 1];
     tform = affine3d(A);
     pc0 = pctransform(pc0, tform);
     pc45 = pctransform(pc45, tform);
@@ -143,49 +143,49 @@ function pcFinal = stitch(I0, I45, I90, I135, I180, I225, I270, I315)
          
     %translate 45 deg point cloud
     x = total(1,1) - total(2,1);
-    y = total(1,3) - total(2,3);
+    z = total(1,3) - total(2,3);
     pcInput = pc45;
-    pcFinal = pc0;
-    pcFinal = pcMinMerge(x, y, pcInput, pcFinal);
+    pcStitched = pc0;
+    pcStitched = pcMinMerge(x, z, pcInput, pcStitched);
 
     %translate 90 deg point cloud
     x = total(1,1) - total(3,1);
-    y = total(1,3) - total(3,3);
+    z = total(1,3) - total(3,3);
     pcInput = pc90;
-    pcFinal = pcMinMerge(x, y, pcInput, pcFinal);
+    pcStitched = pcMinMerge(x, z, pcInput, pcStitched);
 
     %translate 135 deg point cloud
     x = total(1,1) - total(4,1);
-    y = total(1,3) - total(4,3);
+    z = total(1,3) - total(4,3);
     pcInput = pc135;
-    pcFinal = pcMinMerge(x, y, pcInput, pcFinal);
+    pcStitched = pcMinMerge(x, z, pcInput, pcStitched);
 
     %translate 180 deg point cloud
     x = total(1,1) - total(5,1);
-    y = total(1,3) - total(5,3);
+    z = total(1,3) - total(5,3);
     pcInput = pc180;
-    pcFinal = pcMinMerge(x, y, pcInput, pcFinal);
+    pcStitched = pcMinMerge(x, z, pcInput, pcStitched);
     
     %translate 225 deg point cloud
     x = total(1,1) - total(6,1);
-    y = total(1,3) - total(6,3);
+    z = total(1,3) - total(6,3);
     pcInput = pc225;
-    pcFinal = pcMinMerge(x, y, pcInput, pcFinal);
+    pcStitched = pcMinMerge(x, z, pcInput, pcStitched);
 
     %translate 270 deg point cloud
     x = total(1,1) - total(7,1);
-    y = total(1,3) - total(7,3);
+    z = total(1,3) - total(7,3);
     pcInput = pc270;
-    pcFinal = pcMinMerge(x, y, pcInput, pcFinal);
+    pcStitched = pcMinMerge(x, z, pcInput, pcStitched);
     
     %translate 315 deg point cloud
     x = total(1,1) - total(8,1);
-    y = total(1,3) - total(8,3);
+    z = total(1,3) - total(8,3);
     pcInput = pc315;
-    pcFinal = pcMinMerge(x, y, pcInput, pcFinal);
+    pcStitched = pcMinMerge(x, z, pcInput, pcStitched);
     
     %remove noise from merged cloud
-    for i = 1 : 5
-        pcFinal = pcdenoise(pcFinal);
-    end
+     for i = 1 : 5
+         pcStitched = pcdenoise(pcStitched);
+     end
 end
